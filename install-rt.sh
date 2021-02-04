0c1f1cf0b321c58e2420e77bedc7f33a1d6b9e99
Davis Phillips <davis.phillips@gmail.com>
2021-02-04 10:15:17 -0600

    Update install-rt.sh

--- install-rt.sh
+++ install-rt.sh
@@ -1,8 +1,10 @@
 export WS=http://brew-task-repos.usersys.redhat.com/repos/scratch/daolivei/kernel-rt/4.18.0/193.39.1.rt13.89.el8_2.ocptest.2/x86_64/
 
-rpm-ostree install \
-${WS}/kernel-rt-core-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
-${WS}/kernel-rt-kvm-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
-${WS}/kernel-rt-modules-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
-${WS}/kernel-rt-modules-extra-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
-${WS}/kernel-rt-modules-internal-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm
+#rpm-ostree install \
+#${WS}/kernel-rt-core-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
+#${WS}/kernel-rt-kvm-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
+#${WS}/kernel-rt-modules-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
+#${WS}/kernel-rt-modules-extra-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm \
+#${WS}/kernel-rt-modules-internal-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm
+
+rpm-ostree override remove kernel{,-core,-modules,-modules-extra} --install ${WS}/kernel-rt-core-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm --install ${WS}/kernel-rt-modules-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm  --install ${WS}/kernel-rt-modules-extra-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm--install ${WS}/kernel-rt-modules-internal-4.18.0-193.39.1.rt13.89.el8_2.ocptest.2.x86_64.rpm
